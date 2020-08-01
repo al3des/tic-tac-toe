cf60792b85529e3d8b8554fc630369668b8d0018
Alejandro Desalvo <alejandrodesalvo@gmail.com>
2020-08-01 14:28:34 +0200

    added winner check
    added winner check to stop the game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,21 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import useBoard from "../useBoard"
 
 import Square from "./Square"
 
 export default function Board() {
-
+  let [winner, setWinner] = useState(false)
   let [state, dispatchBoard] = useBoard()
   let { board, isXNext} = state
-console.log(state)
+useEffect(()=>{
+  setWinner(checkWinner(board))
+}, [board])
+
   function makeMove(i) {
-    dispatchBoard({type: "MAKE_MOVE", i, board})
+    if(!winner ){
+      dispatchBoard({type: "MAKE_MOVE", i, board})
+    }
+     return
   }
 
   function goBack() {
@@ -37,7 +43,10 @@ console.log(state)
   return (
     <div>
       <h1>Board</h1>
-      <h2>Next Move: {isXNext ? "X" : "O"}</h2>
+      {!checkWinner(board) ?
+      <h2>Next Move: {isXNext ? "X" : "O"}</h2> :
+      <h2>Winner: {winner}</h2>
+      }
       <div style={{ display: "flex", flexWrap: "wrap", width: "200px" }}>
         {renderSquares(board)}
         <div>
@@ -46,4 +55,33 @@ console.log(state)
       </div>
     </div>
   )
+}
+
+function checkWinner(board) {
+  let winnerIndexes = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+  ];
+
+  let winner = "";
+  winnerIndexes.forEach(winnerComb => {
+    if(!winner){
+      winnerComb.reduce((acc, curr, i) => {
+        if (board[acc] !== board[curr]) {
+          winner = false
+          return false
+        }
+        winner = board[curr]
+        return curr;
+      });
+    }
+    return winner;
+  });
+  return winner ? winner : false;
 }
